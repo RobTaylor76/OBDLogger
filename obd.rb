8b99c15bcf29002efd9d3d636401c5a9ea85b2d4
Rob Taylor <Robert_taylor_@hotmail.com>
2013-04-02 23:58:03 +0100

    refactor

--- obd.rb
+++ obd.rb
@@ -2,4 +2,6 @@ require './obd_interface.rb'
 
 obd  = OBDInterface.new("COM3")
 
-obd.readmessage
+obd.connect
+obd.read_message
+obd.diconnect
--- obd_interface.rb
+++ obd_interface.rb
@@ -3,28 +3,43 @@ require 'serialport'
 class OBDInterface
 
   def initialize port, baud = 38400, nbits = 7, stopb = 1
-    @sp = SerialPort.new(port, baud, nbits, stopb, SerialPort::NONE)
+    @port = port
+    @baud = baud
+    @nbits = nbits
+    @stopb = stopb
   end
 
+  #opens the serial port connection
+  def connect
+    disconnect
+    @sp = SerialPort.new(@port, @baud, @nbits, @stopb, SerialPort::NONE)
+  end
+
+  #close the serial port connection
+  def disconnect
+    @sp.close if @sp
+  end
+
+  # Sends a reset message to the OBD interface
   def reset
     at_message 'Z'
   end
 
+  #Instructs the OBD interface to monitor all traffic
   def monitor_all
     at_message 'MA'
   end
 
+  # Send a command to the OBD interface
   def at_message message
     write "AT #{message}"
   end
 
+  #read a single message from the obd interface
   def read_message
     @sp.readline
   end
 
-  def disconnect
-    @sp.close
-  end
 
   private
   def write(message)
